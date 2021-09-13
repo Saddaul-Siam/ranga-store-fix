5e3d8f2e253ef9c5c0a3319d4939e60581352869
saddaul-siam <saddadul.siam@gmail.com>
2021-09-13 13:16:52 +0600

    update more code

--- js/app.js
+++ js/app.js
@@ -225,7 +225,7 @@ const showProducts = (products) => {
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <p id="ratings"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i></p>
-      <p>10 Ratings</P>
+      <p class="text-primary">10 Ratings</P>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="btn text-white">add to cart</button>
       <button id="details-btn" class="btn btn-info text-white">Details</button></div>
       `;
